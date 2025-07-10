fad454d06da3a07fd55a4c5058bfecf8be599c71
Zain Jamshaid <zainjamshaid25@gmail.com>
2025-07-10 06:07:02 +0500

    Created featured product page

---
+++ src/app/collections/featured/page.jsx
@@ -0,0 +1,33 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import ProductsDisplay from '../components/ProductsDisplay';
+
+const FeaturedPage = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('/api/products/featured');
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <ProductsDisplay products={products} title="Featured"/>;
+};
+
+export default FeaturedPage;
+
